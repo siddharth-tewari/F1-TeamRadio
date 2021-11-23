b8a21b7d1d15a65da9ab69c59504ab4f75c81fa8
siddharth-tewari <76590952+siddharth-tewari@users.noreply.github.com>
2021-11-23 13:19:41 +0530

    Added New Features

---
+++ TeamRadio.py
@@ -0,0 +1,35 @@
+import urllib.request as request
+import json
+import requests
+from jsonstream import load
+from io import StringIO
+import logging
+
+def radio():
+    print("Team Radio")
+    with request.urlopen('http://livetiming.formula1.com/static/SessionInfo.json') as response:
+        if response.getcode() == 200:
+                logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+                logging.info('Getting the URL')
+                source = response.read()
+                data = json.loads(source)
+                location = "http://livetiming.formula1.com/static/" + data['Path'] + "TeamRadio.json"
+                logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+                logging.info('URL Successfully Found')
+                url = location
+                print(url)
+                r = requests.get(location)
+                r.encoding = 'utf-8-sig'
+                out = json.loads(r.text)
+                print(json.dumps(out, indent = 4))
+                output = json.dumps(out, indent = 4)
+                
+        else:
+            print('Unknown Error Occured')
+
+radio()
+
+
+
+
+
